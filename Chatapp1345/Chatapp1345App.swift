f3c0e458fbb38d9621975f8f5527d4b3b65b845a
orukahairuka <sakuraierika03301109@gmail.com>
2024-03-14 10:50:07 +0900

    Initial Commit

---
+++ Chatapp1345/Chatapp1345App.swift
@@ -0,0 +1,17 @@
+//
+//  Chatapp1345App.swift
+//  Chatapp1345
+//
+//  Created by 櫻井絵理香 on 2024/03/14.
+//
+
+import SwiftUI
+
+@main
+struct Chatapp1345App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
