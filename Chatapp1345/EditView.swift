2d69c7b9c401c4e3b1b2d0c6800d415e2911c523
orukahairuka <sakuraierika03301109@gmail.com>
2024-03-14 23:12:40 +0900

    記事書くがめん作った

--- Chatapp1345/EditView.swift
+++ Chatapp1345/EditView.swift
@@ -7,21 +7,42 @@
 
 import SwiftUI
 
-struct EditView: View {
+//モーダルのテスト用View
+struct ModalTestView: View {
+    @State var isShowSheet = false
     var body: some View {
-        NavigationView {
-            Text("aaa")
-            .toolbar{
-                HStack {
-                    Text("テキスト")
-                    Button("ボタン"){}
-                    NavigationLink("ナビゲーションリンク") {}
-                }
-            }
+        Button("シートを表示") {
+            isShowSheet.toggle()
+        }
+        .sheet(isPresented: $isShowSheet){
+            EditView()
         }
     }
 }
 
+struct EditView: View {
+    @State var inputTitle = ""
+    @State var inputDetail = ""
+    var body: some View {
+        VStack {
+            Button {
+
+            } label: {
+                Text("投稿する")
+            }
+            TextField("タイトル", text: $inputTitle)
+                .textFieldStyle(.roundedBorder)
+                .frame(width: 355,height: 40)
+                .padding(.all)
+            TextEditor(text: $inputDetail)
+                .frame(width: 355,height: 380)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5) // 角が丸い四角形をオーバーレイとして追加
+                        .stroke(.gray, lineWidth: 1) // このオーバーレイに枠線を適用
+                )        }
+    }
+}
+
 #Preview {
-    EditView()
+    ModalTestView()
 }
